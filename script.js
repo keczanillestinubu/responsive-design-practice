526a790894fdb9a2100b707aa330e513ace15fb0
illeskeczan <illes.keczan@innoveo.com>
2026-03-30 14:21:17 +0200

    add styles, implement validation on the input

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ async function getPicture(){
     const dogApiUrl = "https://dog.ceo/api/breeds/image/random";
     const response = await fetch(dogApiUrl);
     const dogImage = await response.json();
-    const dogElement = document.getElementById("dogPicture");
+    const dogElement = document.createElement("img");
     dogElement.src = dogImage.message;
+    dogElement.alt = "dog picture";
+    const container = document.getElementById("singleImage");
+    container.innerHTML= "";
+    container.appendChild(dogElement);
 }
 
 async function getMultiplePictures(){
     const dogApiUrl = "https://dog.ceo/api/breeds/image/random";
     const input = document.getElementById("pictureNumber");
     const number = Number(input.value);
-    let imageContainer = document.getElementById("imageContainer");
+    const imageContainer = document.getElementById("imageContainer");
     imageContainer.innerHTML = "";
-    
+
     for (let index = 0; index < number; index++) {
         const response = await fetch(dogApiUrl);
         const dogImage = await response.json();
@@ -25,6 +29,13 @@ async function getMultiplePictures(){
 }
 
 function main(){
+    const input = document.getElementById("pictureNumber");
+
+    input.addEventListener("input", () => {
+        if (Number(input.value) > 10) {
+            input.value = 10;
+        }
+    });
     const getPictureButton = document.getElementById("getPictures");
     getPictureButton.addEventListener("click", getPicture);
     const getMultiplePicturesButton = document.getElementById("getCustomPictures");
