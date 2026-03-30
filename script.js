c49b1b073154ab5218c41c5d755f0088b6712fb1
illeskeczan <illes.keczan@innoveo.com>
2026-03-30 14:04:26 +0200

    create javascript for multiple pictures

--- script.js
+++ script.js
@@ -6,8 +6,28 @@ async function getPicture(){
     dogElement.src = dogImage.message;
 }
 
+async function getMultiplePictures(){
+    const dogApiUrl = "https://dog.ceo/api/breeds/image/random";
+    const input = document.getElementById("pictureNumber");
+    const number = Number(input.value);
+    let imageContainer = document.getElementById("imageContainer");
+    imageContainer.innerHTML = "";
+    
+    for (let index = 0; index < number; index++) {
+        const response = await fetch(dogApiUrl);
+        const dogImage = await response.json();
+        const img = document.createElement("img");
+        img.src = dogImage.message;
+        img.alt = "dog picture";
+        const container = document.getElementById("imageContainer");
+        container.appendChild(img)
+    }
+}
+
 function main(){
     const getPictureButton = document.getElementById("getPictures");
-    getPictureButton.addEventListener("click", getPicture)
+    getPictureButton.addEventListener("click", getPicture);
+    const getMultiplePicturesButton = document.getElementById("getCustomPictures");
+    getMultiplePicturesButton.addEventListener("click", getMultiplePictures);
 }
 main()
