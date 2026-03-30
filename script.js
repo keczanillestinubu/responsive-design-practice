66bcc26857e31148169c9385531db8a06283ab9d
illeskeczan <illes.keczan@innoveo.com>
2026-03-30 13:32:34 +0200

    create buttons to get pictures, initial javascript functions

---
+++ script.js
@@ -0,0 +1,9 @@
+function getPicture(){
+    
+}
+
+function main(){
+    const getPictureButton = document.getElementById("getPictures");
+    getPictureButton.addEventListener("click", getPicture())
+}
+main()
