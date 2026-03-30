57002bef5ba99b5ff7c61747ff75ba73281dc980
illeskeczan <illes.keczan@innoveo.com>
2026-03-30 13:42:06 +0200

    create javascript to get one picture

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-function getPicture(){
-    
+async function getPicture(){
+    const dogApiUrl = "https://dog.ceo/api/breeds/image/random";
+    const response = await fetch(dogApiUrl);
+    const dogImage = await response.json();
+    const dogElement = document.getElementById("dogPicture");
+    dogElement.src = dogImage.message;
 }
 
 function main(){
     const getPictureButton = document.getElementById("getPictures");
-    getPictureButton.addEventListener("click", getPicture())
+    getPictureButton.addEventListener("click", getPicture)
 }
 main()
